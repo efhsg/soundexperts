d8194eb8e5e41d7f0787cff20d976b1907b0a550
esg <efhsg@live.nl>
2022-07-03 23:30:53 +0200

    stripped docker compose, fix install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,7 @@
 cp .env.example .env && \
 docker run --rm --interactive --tty --volume $PWD:/app --volume ${COMPOSER_HOME:-$HOME/.composer}:/tmp composer install && \
 vendor/bin/sail up -d && \
+sleep 15s && \
 vendor/bin/sail artisan key:generate && \
 vendor/bin/sail artisan migrate
+sudo chown -R $USER ./vendor/
